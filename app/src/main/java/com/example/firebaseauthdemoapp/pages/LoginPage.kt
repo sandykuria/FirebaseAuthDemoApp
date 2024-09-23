a9650cb896c1a93a54bf751c42a40f9bee7c8492
Sandra <kuriasandra651@gmail.com>
2024-09-23 19:22:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaseauthdemoapp/pages/LoginPage.kt
@@ -0,0 +1,9 @@
+package com.example.firebaseauthdemoapp.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+
+}
